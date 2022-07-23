36e3857ad70363f5c9f8002bae1bf3c884b8fe5c
Tox-XStarWake <toxicsalt0001@gmail.com>
2022-07-23 19:23:49 -0400

    updated

--- login-page.js
+++ login-page.js
@@ -4,10 +4,11 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const username = loginForm.username.value;
+//    const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "user" && password === "web_dev") {
+//    if (username === "user" && password === "web_dev") {
+    if (password === "web_dev") {        
         alert("You have successfully logged in.");
         location.reload();
     } else {
